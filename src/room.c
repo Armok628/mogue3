58b92924c6d1d742be13406f0a0d8e715ab7f356
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-03-29 22:06:24 -0500

    Generalized room generation to any area

--- src/room.c
+++ src/room.c
@@ -1,50 +1,50 @@
 #include "room.h"
-void make_door(int x,int y)
+void make_door(tile_t *a,int x,int y)
 {
 	int l=lin(x,y);
-	local_area[l].fg='+';
-	local_area[l].fg_c=BROWN;
-	local_area[l].bg='-';
-	local_area[l].bg_c=BROWN;
+	a[l].fg='+';
+	a[l].fg_c=BROWN;
+	a[l].bg='-';
+	a[l].bg_c=BROWN;
 }
-void make_room(int x,int y,int width,int height,dir_t direction)
+void make_room(tile_t *a,int x,int y,int w,int h,dir_t direction)
 {
-	for (int xo=0;xo<width;xo++)
-		for (int yo=0;yo<height;yo++) {
+	for (int xo=0;xo<w;xo++)
+		for (int yo=0;yo<h;yo++) {
 			int l=lin(x+xo,y+yo);
-			local_area[l].fg='\0';
-			local_area[l].bg='#';
-			local_area[l].bg_c=LGRAY;
+			a[l].fg='\0';
+			a[l].bg='#';
+			a[l].bg_c=LGRAY;
 		}
-	for (int i=0;i<width;i++) {
-		int l1=lin(x+i,y),l2=lin(x+i,y+height-1);
-		local_area[l1].fg='%';
-		local_area[l1].fg_c=DGRAY;
-		local_area[l2].fg='%';
-		local_area[l2].fg_c=DGRAY;
+	for (int i=0;i<w;i++) {
+		int l1=lin(x+i,y),l2=lin(x+i,y+h-1);
+		a[l1].fg='%';
+		a[l1].fg_c=DGRAY;
+		a[l2].fg='%';
+		a[l2].fg_c=DGRAY;
 	}
-	for (int i=0;i<height;i++) {
-		int l1=lin(x,y+i),l2=lin(x+width-1,y+i);
-		local_area[l1].fg='%';
-		local_area[l1].fg_c=DGRAY;
-		local_area[l2].fg='%';
-		local_area[l2].fg_c=DGRAY;
+	for (int i=0;i<h;i++) {
+		int l1=lin(x,y+i),l2=lin(x+w-1,y+i);
+		a[l1].fg='%';
+		a[l1].fg_c=DGRAY;
+		a[l2].fg='%';
+		a[l2].fg_c=DGRAY;
 	}
 	switch (direction) {
 	case NORTH:
-		make_door(x+width/2,y);
+		make_door(a,x+w/2,y);
 		break;
 	case SOUTH:
-		make_door(x+width/2,y+height-1);
+		make_door(a,x+w/2,y+h-1);
 		break;
 	case EAST:
-		make_door(x+width-1,y+height/2);
+		make_door(a,x+w-1,y+h/2);
 		break;
 	case WEST:
-		make_door(x,y+height/2);
+		make_door(a,x,y+h/2);
 	}
 }
-void random_room()
+void random_room(tile_t *a)
 {
 	int x=1+rand()%(WIDTH-1);
 	int y=1+rand()%(HEIGHT-1);
@@ -54,5 +54,5 @@ void random_room()
 		x-=w;
 	if (y+h>HEIGHT)
 		y-=h;
-	make_room(x,y,w,h,(dir_t)rand()%4);
+	make_room(a,x,y,w,h,(dir_t)rand()%4);
 }
--- src/room.h
+++ src/room.h
@@ -4,6 +4,6 @@
 
 typedef enum {NORTH,SOUTH,EAST,WEST} dir_t;
 
-void make_room(int,int,int,int,dir_t);
-void random_room();
+void make_room(tile_t *,int,int,int,int,dir_t);
+void random_room(tile_t *);
 #endif
