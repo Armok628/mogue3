2a948fe649932fd55be220bc826d93b2c25382c1
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-07-06 02:39:55 -0500

    Adjustment to thinking function

--- src/think.c
+++ src/think.c
@@ -5,7 +5,7 @@ char think(entity_t *e)
 		return '5'; // Wait
 	if ((rand()%100)<e->wis)
 		return 'm'; // Cast spell
-	char mov_opts[9],atk_opts[9];
+	int mov_opts[9],atk_opts[9];
 	int n_atks=0,n_movs=0;
 	int coords=e->coords;
 	for (int dx=-1;dx<=1;dx++)
@@ -15,18 +15,21 @@ char think(entity_t *e)
 			int c=coords+lin(dx,dy);
 			if (!legal_move(coords,c)) // Illegal move
 				continue;
-			if (local_area[c].fg&&e->flags&SOLID) // Solid self and wall
+			char f=local_area[c].fg;
+			if (f&&f!='+'&&e->flags&SOLID) // Non-door wall and solid self
 				continue;
 			entity_t *t=local_area[c].e;
 			if (!t||!friend(e,t)||enemy(e,t)) // Absent, nonfriend, or enemy
-				mov_opts[n_movs++]=offset_input(c-coords); // Possible move
+				mov_opts[n_movs++]=c; // Possible move
 			else if (t&&enemy(e,t)&&!friend(e,t)) // Present and nonfriend enemy
-				atk_opts[n_atks++]=offset_input(c-coords); // Possible attack
+				atk_opts[n_atks++]=c; // Possible attack
 		}
+	int dest;
 	if (n_atks)
-		return atk_opts[rand()%n_atks];
+		dest=atk_opts[rand()%n_atks];
 	else if (n_movs)
-		return mov_opts[rand()%n_movs];
-	// If all else fails for some reason, do anything
-	return generate_input();
+		dest=mov_opts[rand()%n_movs];
+	else
+		dest=generate_input();
+	return offset_input(dest-coords);
 }
